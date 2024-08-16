c7326fc2ecead84dbc6193349f4d25b61febf4db
uvenven <fadilefd1102@gmail.com>
2024-08-16 14:21:06 +0700

    fix:memperbaiki hilangnya parameter i pada footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,22 +7,18 @@ const Footer = () => {
       <div className="screen-max-width">
         <div>
           <p className="font-semibold text-gray text-xs">
-            More ways to shop:
-            <span className="underline text-blue">Find an Apple Store</span>
-            or
-            <span className="underline text-blue">other retailer</span>
-            near you.
+            More ways to shop: <span className="underline text-blue">Find an Apple Store</span> or <span className="underline text-blue">other retailer</span> near you.
           </p>
           <p className="font-semibold text-gray text-xs">Or call 000800-049-1998</p>
         </div>
 
         <div className="bg-neutral-700 my-5 h-[1px] w-full" />
         <div className="flex md:flex-row flex-col md:items-center justify-between">
-          <p className="font-semibold text-gray text-xs">Copyright @ 2024 Apple Inc. All rights reversed.</p>
+          <p className="font-semibold text-gray text-xs">Copyright @ 2024 Apple Inc. All rights reserved.</p>
           <div className="flex">
-            {footerLinks.map((link) => (
+            {footerLinks.map((link, i) => (
               <p key={link} className="font-semibold text-gray text-xs">
-                {link} {i != footerLinks.length - 1 && <span className="mx-2"> | </span>}
+                {link} {i !== footerLinks.length - 1 && <span className="mx-2"> | </span>}
               </p>
             ))}
           </div>
